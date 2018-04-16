614fce3f57526b90b743621600aa59e09a37b0cb
Dameon1 <dameonmedoza@gmail.com>
2018-04-16 12:49:52 -0700

    first set done

--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 'use strict';
 
 const data = require('./db/notes');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/api/notes/:id', (req,res) => {
+  console.info(req.params);
+  let id = Number(req.params.id);
+  let item = data.find(item => item.id === id);
+  res.json(item);
+  console.info(id);
+});
+
+
+app.get('/api/notes', (req, res) => {
+  let searchTerm = req.query.searchTerm;
+  let itemsToReturn = data.filter(item => item.title.includes(searchTerm));
+  
+  res.json(itemsToReturn);
+  
+});
+
+app.listen(8080,function(){
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error',error => {
+  console.error(error);
+});
 
 console.log('Hello Noteful!');
 
