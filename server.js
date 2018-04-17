4dfcb9ff561f083e619bf9ff403d7374bd737cb2
Dameon1 <dameonmedoza@gmail.com>
2018-04-17 13:21:52 -0700

    500 added

--- server.js
+++ server.js
@@ -30,11 +30,29 @@ app.get('/api/notes/:id', (req, res) => {
 
 });
 
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+
+
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   res.status(404).json({ message: 'Not Found' });
 });
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+
+
+
 
 // Listen for incoming connections
 app.listen(PORT, function () {
@@ -42,6 +60,7 @@ app.listen(PORT, function () {
 }).on('error', err => {
   console.error(err);
 });
+
 
 
 
